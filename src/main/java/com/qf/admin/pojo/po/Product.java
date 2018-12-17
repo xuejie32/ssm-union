444a567d4251ff39063e7032d3fdc5e96f0f1a7e
49456 <494568364@qq.com>
2018-12-17 20:51:51 +0800

    测试

---
+++ src/main/java/com/qf/admin/pojo/po/Product.java
@@ -0,0 +1,13 @@
+package com.qf.admin.pojo.po;
+
+public class Product {
+    private int pid;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+}
